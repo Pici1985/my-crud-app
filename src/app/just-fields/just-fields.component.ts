2440cb52bfa14d4786051ed899692bb8fff18ba8
Balazs Ferencz <64424862+Pici1985@users.noreply.github.com>
2022-04-03 07:01:03 +0100

    api call works in justfields

--- src/app/just-fields/just-fields.component.ts
+++ src/app/just-fields/just-fields.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-just-fields',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class JustFieldsComponent implements OnInit {
 
-  constructor() { }
+  // ide: https://justfields.com/project/eXrglmyB/json 
+
+  title = 'JustFields';
+
+  public entries!: any;
+
+  public getData(): Observable<any> {
+    return this.http.get<any>(`https://justfields.com/project/eXrglmyB/json`);
+  }
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.getData().subscribe(res => {
+      this.entries = res;
+      console.log(this.entries);
+    });
   }
 
 }
